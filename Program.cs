51626dcf3b9391b9ab496edf9afefca5be7eeeae
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2022-03-08 10:49:43 +0100

    fixade skjutningen

--- Program.cs
+++ Program.cs
@@ -5,27 +5,31 @@ namespace Vinter_Projekt
 {
     class Program
     {
-        static bool hasShot = false;
-        static Rectangle bullet = new Rectangle();
+        static int screenW = 800;
+        static int screenH = 600;
 
         static void Main(string[] args)
         {
-            const int fönsterB = 800;
-            const int fönsterH = 600;
+            Random generator = new Random();
+            bool hasShot = false;
 
+            //Spelarens position
             int posX = 200;
             int posY = 300;
 
-            Random generator = new Random();
+            //Slumpar fiendens storlek
+            int enemySize = generator.Next(15, 35);
 
-            int enemySize = generator.Next(15, 35); ;
-            int enemyX = generator.Next(enemySize, fönsterB - enemySize * 2);
+            //Slumpar fiendens position i x-led
+            int enemyX = generator.Next(enemySize, screenW - enemySize * 2);
+
+            //Slumpar fiendens hastighet
             int enemySpeed = generator.Next(2, 6);
             int enemyY = 0;
 
             //Texture2D spaceship = Raylib.LoadTexture(@"Resurser/spaceship.png");
 
-            Raylib.InitWindow(fönsterB, fönsterH, "Flappy Bird");
+            Raylib.InitWindow(screenW, screenH, "Space Shooter 76");
             Raylib.SetTargetFPS(60);
 
             // Animationsloopen
@@ -35,23 +39,46 @@ namespace Vinter_Projekt
 
                 Raylib.ClearBackground(Color.LIGHTGRAY);
 
-                Shoot(posX, posY, enemySize);
+                //Create player
+                Rectangle player = new Rectangle(posX, posY, 25, 25);
+                
+                //Create enemy
+                Rectangle enemy = new Rectangle(enemyX, enemyY, enemySize, enemySize);
 
-                Raylib.DrawRectangle(posX, posY, 25, 25, Color.BLUE);
+
+                //Move player
+                Move(ref posX, ref posY);
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
+                {
+                    hasShot = true;
+                }
+
+                if (hasShot)
+                {
+                    float bulletPosY = Bullet.Shoot(posX, posY);
+
+                    if (bulletPosY < 1 || Raylib.CheckCollisionRecs(player, enemy))
+                    {
+                        hasShot = false;
+                    }
+                }
+
 
                 //Raylib.DrawTexture(spaceship, 200, 300, Color.BLACK);
+                Raylib.DrawRectangleRec(player, Color.BLUE);
 
+                //Draw enemy
+                Raylib.DrawRectangleRec(enemy, Color.BLACK);
+
+                //Move enemies
                 enemyY += enemySpeed;
-
-                Drive(ref posX, ref posY);
-
-                Raylib.DrawRectangle(enemyX, enemyY, enemySize, enemySize, Color.BLACK);
 
                 Raylib.EndDrawing();
             }
         }
 
-        static void Drive(ref int posX, ref int posY)
+        static void Move(ref int posX, ref int posY)
         {
             int pullForce = 3;
             int speed = 6;
@@ -84,28 +111,42 @@ namespace Vinter_Projekt
                 posX -= speed;
             }
         }
+    }
 
-        static void Shoot(int posX, int posY, int enemySize)
+    class Bullet
+    {
+        static int bulletY = 0;
+        static int bulletX = 0;
+
+        static bool check = false;
+
+        public static float Shoot(int posX, int posY)
         {
-            int bulletSpeed = 10;
-
-            if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+            if (bulletY > 0)
             {
-                hasShot = true;
-            }
+                int bulletWidth = 10;
+                int bulletHeight = 15;
+                int bulletSpeed = 10;
 
-            if (hasShot)
-            {
-                Raylib.DrawRectangleRec(bullet, Color.DARKPURPLE);
-                bullet.y -= bulletSpeed;
+                if (!check)
+                {
+                    //Sätter skottets position lika med spelarens position
+                    bulletX = posX;
+                    bulletY = posY;
+                    check = true;
+                }
+
+                //Ritar skottet och flyttar det
+                Raylib.DrawRectangle(bulletX + bulletWidth / 2, bulletY, bulletWidth, bulletHeight, Color.GOLD);
+                bulletY -= bulletSpeed;
             }
             else
             {
-                bullet.x = posX + enemySize / 4;
-                bullet.y = posY;
-                bullet.width = 10;
-                bullet.height = 15;
+                bulletX = posX;
+                bulletY = posY;
             }
+
+            return bulletY;
         }
     }
 }
